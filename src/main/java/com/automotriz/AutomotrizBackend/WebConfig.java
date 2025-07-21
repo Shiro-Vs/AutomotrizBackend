c65b2d313001bc174994744a4bee89fa912a8bd6
ShiroVs <robert.vs@outlook.com>
2025-07-21 12:47:45 -0500

    URL de frontend cambiado

--- src/main/java/com/automotriz/AutomotrizBackend/WebConfig.java
+++ src/main/java/com/automotriz/AutomotrizBackend/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173",
-                        "https://automotriz-production.up.railway.app") // 🔁 cambia esto si tu frontend usa otro puerto
+                        "https://safservice.up.railway.app") // 🔁 cambia esto si tu frontend usa otro puerto
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
