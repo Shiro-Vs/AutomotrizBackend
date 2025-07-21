e860a1d90774236d045f0a694248c016a925b9a2
ShiroVs <robert.vs@outlook.com>
2025-07-20 21:53:46 -0500

    Configuracion para despliege

--- src/main/java/com/automotriz/AutomotrizBackend/WebConfig.java
+++ src/main/java/com/automotriz/AutomotrizBackend/WebConfig.java
@@ -9,8 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173") // 🔁 cambia esto si tu frontend usa otro puerto
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*");
+                .allowedOrigins("http://localhost:5173",
+                        "automotriz-production.up.railway.app") // 🔁 cambia esto si tu frontend usa otro puerto
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
